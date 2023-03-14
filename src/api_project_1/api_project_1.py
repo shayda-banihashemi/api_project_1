d397dd0cda3169525e5ea9b914f24eecadd3bd91
shayda-banihashemi <shayda.dev@icloud.com>
2023-03-14 14:06:02 -0700

    added script to project

---
+++ src/api_project_1/api_project_1.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
+from sqlalchemy import create_engine, text
+
+db_connect = create_engine("sqlite:///chinook.db")
+
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute(text("select * from employees"))
+        result = {"employees": [i[0] for i in query.cursor.fetchall()]}
+        conn.close()
+        return jsonify(result)
+
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        sql = text("select trackid, name, composer, unitprice from tracks;")
+        query = conn.execute(sql)
+        result = {"data": [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        conn.close()
+        return jsonify(result)
+
+
+class EmployeesName(Resource):
+    def get(self, employee_id: int):
+        conn = db_connect.connect()
+        query = conn.execute(
+            text("select * from employees where EmployeeId =%d " % int(employee_id))
+        )
+        result = {"data": [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        conn.close()
+        return jsonify(result)
+
+
+def main():
+    app = Flask(__name__)
+
+    api = Api(app)
+    api.add_resource(Employees, "/employees")  # Route_1
+    api.add_resource(Tracks, "/tracks")  # Route_2
+    api.add_resource(EmployeesName, "/employees/<employee_id>")  # Route_3
+
+    app.run(port="5000")
+
+    db_connect.dispose()
+
+
+if __name__ == "__main__":
+    main()
